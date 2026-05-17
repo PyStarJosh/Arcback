823c42fa908df45b6f96a1fcf78330a8de6026a8
Joshua J. <jjack05@protonmail.com>
2026-05-16 22:41:25 -0400

    feat(strategy): MA Crossover strategy implemented

--- src/backend/strategy/algorithms.py
+++ src/backend/strategy/algorithms.py
@@ -1,18 +1,30 @@
 import pandas as pd
 from .indicators import Indicators
 
+
 class Algorithms:
-    '''Startegy Implementation'''
-    
-    def __init__(self, df):
+    """Startegy Implementation"""
+
+    def __init__(self):
         self.indicators = Indicators()
-        
-    def ma_crossover(self, df: pd.DataFrame, short_period: int, long_period: int) -> pd.DataFrame:  
+
+    def ma_crossover(self, df: pd.DataFrame, short_period: int, long_period: int) -> pd.DataFrame:
+        # creates a copy of passed DataFrame data structure and creates 3 columns
+        # 2 columns hold the returned series from the ma indicators and the last one hold the
+        # buy or sell signals that will be used by engine.py
         result = df.copy()
-        result['ema_short'] = self.indicators(df['closes'], short_period)
-        result['sma_long'] = self.indicators(df['closes', long_period])   
-        result['signal'] = 0
-        
-        # buy_crossing = (df['ema_short'] > long_sma) & (short_ema.shift(1) <= long_sma.shift(1))
-        # sell_crossing = (short_ema < long_sma) & (short_ema.shift(1) >= long_sma.shift(1))
-        
+        result["ema_short"] = self.indicators.ema(result["closes"], short_period)
+        result["sma_long"] = self.indicators.sma(result["closes"], long_period)
+        result["signal"] = 0  # init pandas series with default values of 0
+        ema_short, sma_long = result["ema_short"], result["sma_long"]
+
+        # Provides boolean values that are used for buy or sell signals
+        is_bullish, is_bearish = ema_short > sma_long, ema_short < sma_long
+        buy_crossing = (is_bullish) & (ema_short.shift(1) <= sma_long.shift(1))
+        sell_crossing = (is_bearish) & (ema_short.shift(1) >= sma_long.shift(1))
+
+        # Result signal column population
+        result.loc[buy_crossing, "signal"] = 1
+        result.loc[sell_crossing, "signal"] = -1
+
+        return result
--- src/backend/strategy/indicators.py
+++ src/backend/strategy/indicators.py
@@ -7,7 +7,7 @@ class Indicators:
     def sma(self, price_data: pd.Series, period: int) -> pd.Series:
         return price_data.rolling(window=period).mean()
     
-    # Takes price_data['close] value, creates a exponentially weighted window view of this data eith teh smoothing
+    # Takes price_data['close] value, creates a exponentially weighted window view of this data with the smoothing
     # factor of a N-period EMA followed by using the recursive forumla a * P(t) + (1-a) * EMA(P(-t))
     def ema(self, price_data: pd.Series, period: int) -> pd.Series:
         return price_data.ewm(span=period, adjust=False).mean()
