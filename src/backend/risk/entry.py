022b50766b9e09fa45207197d84625987d05f2aa
Joshua J. <jjack05@protonmail.com>
2026-05-25 13:03:51 -0400

    feat(risk): implemented set_tp and set_sl methods

--- src/backend/risk/entry.py
+++ src/backend/risk/entry.py
@@ -7,3 +7,15 @@ class Entry:
     @staticmethod
     def check_entry(timestamp: pd.DatetimeIndex, entry_indicator: pd.DataFrame) -> bool:
         return entry_indicator.at[timestamp, 'signal'] == 1
+    
+    @staticmethod
+    def set_tp(side: int, entry_price: float, atr: float, multiplier: float) -> float:
+        if side == 1:
+            return entry_price + (atr * multiplier)
+        return entry_price - (atr * multiplier)
+
+    @staticmethod
+    def set_sl(side: int, entry_price: float, atr: float, multiplier: float) -> float:
+        if side == 1:
+            return entry_price - (atr * multiplier)
+        return entry_price + (atr * multiplier)
