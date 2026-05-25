e5e40f8106b1507424aa48c9d200d8e5b052a298
Joshua J. <jjack05@protonmail.com>
2026-05-25 08:37:09 -0400

    feat(portfolio): implemented portfolio class with method for adjusting equity

--- src/backend/portfolio/portfolio.py
+++ src/backend/portfolio/portfolio.py
@@ -1,22 +1,19 @@
-# import pandas as pd
+import pandas as pd
 
-# class Portfolio:
-#     '''Emulates & executes the properties and operations of a investment portfolio account'''
+class Portfolio:
+    '''Emulates & executes the properties and operations of a investment portfolio account'''
     
+    def __init__(self, equity: float, dt_index: pd.DatetimeIndex) -> None:
+        if equity <= 0:
+            raise ValueError('The balance must be greater than $0')
+        self.equity = equity
+        self.portfolio = pd.DataFrame(
+            {
+                'price': self.equity,
+            }, index= dt_index
+        )
     
-#     def __init__(self, balance: float, dt_index: pd.DatetimeIndex) -> None:
-#         if balance <= 0:
-#             raise ValueError('The balance must be greater than $0')
-#         self.balance = balance
-#         self.portfolio = pd.DataFrame(
-#             {
-#                 'price': self.balance,
-#             }, index= dt_index
-#         )
-    
-#     def adjust_balance(self, amount: float, datetime: pd.DatetimeIndex) -> pd.DataFrame:
-#         '''adjusts the portfolio's balance based on passed float amount'''
-#         self.balance += amount
-#         self.portfolio.loc[datetime, 'price'] = self.balance
-#         self.portfolio.loc[datetime, 'frac_change'] = self.portfolio.loc[datetime, 'pct_change'] / self.portfolio.loc[datetime, 'price'].shift(1)
-#         return self.portfolio
+    def adjust_qruity(self, amount: float) -> float:
+        '''adjusts the portfolio's balance based on passed float amount'''
+        self.equity += amount
+        return self.equity
