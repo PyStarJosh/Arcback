37c38b0757ddc284022699611d1f7535d3536f64
Joshua J. <jjack05@protonmail.com>
2026-05-25 08:38:15 -0400

    feat(risk): implemented an entry class for trade execution

--- src/backend/risk/entry.py
+++ src/backend/risk/entry.py
@@ -1,2 +1,9 @@
+import pandas as pd
+from dataclasses import dataclass
+from ..strategy.algorithms import Algorithms
+    
 class Entry:
-    ''''''
+    '''Executes trade entry based on configured entry criteria'''
+    
+    def check_entry(self, timestamp: pd.DatetimeIndex, entry_indicator: pd.DataFrame) -> bool:
+        return entry_indicator.at[timestamp, 'signal'] == 1
--- src/backend/risk/limits.py
+++ src/backend/risk/limits.py
@@ -1,7 +1,6 @@
 class Limits:
     '''Measure Trade Risk Exposure'''
     
-    
     @staticmethod
     def valid_exposure(max_exposure: float, equity: float, proposed_trade_size: float) -> bool:
         '''Returns a bool value stating if the proposed_trade_size is within per-trade max_exposure'''
